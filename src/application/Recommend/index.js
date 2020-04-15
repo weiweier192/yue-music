57671553ee8f6f7d82ff91ab690c15fe70591614
weiweier192 <Aweiweier192@163.com>
2020-04-16 00:58:27 +0800

    '开发轮播组件'

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Slider from '../../components/slider'
+
 function Recommend (props) {
+  // mock数据
+  const bannerList = [1, 2, 3, 4].map(item => {
+    return {imageUrl: "http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg"}
+  })
   return (
     <div>
-      <div>Recommend</div>
+      <Slider bannerList={bannerList}></Slider>
     </div>
   )
 }
