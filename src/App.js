34c190ce0ae7b98fd69a2a1a3f675c7d88f45ddd
weiweier192 <Aweiweier192@163.com>
2020-04-15 19:47:56 +0800

    '编写路由文件'

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React from 'react';
 import { IconStyle } from './assets/iconfont/iconfont'
 import { GlobalStyle } from './style.js'
 
+import {renderRoutes} from 'react-router-config'
+import routes from './routes/index.js'
+import {HashRouter} from 'react-router-dom'
+
 function App () {
   return (
     <div className="App">
