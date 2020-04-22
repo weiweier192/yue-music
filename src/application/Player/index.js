b7f986a147eabc08f906b404cd890a973e265aac
weiweier192 <Aweiweier192@163.com>
2020-04-22 16:23:50 +0800

    '迷你版布局完成'

--- src/application/Player/index.js
+++ src/application/Player/index.js
@@ -9,10 +9,18 @@ import {
   changePlayingState,
   changeShowPlayList
 } from './store/actionCreators.js'
+import MiniPlayer from './miniPlayer/index.js'
 
 function Player (props) {
+  const currentSong = {
+    al: { picUrl: "https://p1.music.126.net/JL_id1CFwNJpzgrXwemh4Q==/109951164172892390.jpg" },
+    name: "木偶人",
+    ar: [{name: "薛之谦"}]
+  }
   return (
-    <div>Player</div>
+    <div>
+      <MiniPlayer song={currentSong}></MiniPlayer>
+    </div>
   )
 }
 
